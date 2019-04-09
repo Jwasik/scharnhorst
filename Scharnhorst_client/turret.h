8fc7e1229ac6289af1b681dd3d9e37a37001d0c0
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2019-04-09 21:37:04 +0200

    wieżyczki ograniczenie prędkośći obrotu
    powinny działać ale nia mam jak sprawdzić bo serwer nie działa

--- Scharnhorst_client/turret.h
+++ Scharnhorst_client/turret.h
@@ -6,10 +6,12 @@ class turret : public movable
 	
 protected:
 	std::string type;
+	float rotationSpeed; //w stopniach
 	sf::Vector2f shipOrigin;
 	float distanceFromShipOrigin;
 	float angleFromShipOrigin;
 	float lockedArea[2];//k¹rowy obszar w którym turret nie mo¿e przebywaæ (mniejszy k¹t, wiêkszy k¹t) 
+	int middleOfLockedArea;
 	std::vector<sf::Vector2f> barrelPoints; //Miejsca oznaczaj¹ce koñcu luf dzia³, gdzie powinny siê pojawiæ pociski
 	sf::Vector2f position;
 
